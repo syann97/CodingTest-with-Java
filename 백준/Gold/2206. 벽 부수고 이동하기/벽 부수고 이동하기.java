27fdc00254378721adf22f4a783708ba3f7d67de
syann97 <shallom75321@gmail.com>
2025-04-21 23:51:56 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 436 ms, Memory: 80544 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -2,8 +2,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Node {
@@ -29,26 +27,21 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[][][] map = new int[2][N][M];
+        int[][] map = new int[N][M];
 
         for (int i = 0; i < N; i++) {
             String tmp = br.readLine();
             for (int j = 0; j < M; j++) {
-                char c = tmp.charAt(j);
-                map[0][i][j] = c;
-                map[1][i][j] = c;
+                map[i][j] = tmp.charAt(j);
             }
         }
-
         System.out.println(bfs(map, N, M));
-
-
     }
 
-    static int bfs(int[][][] map, int N, int M) {
+    static int bfs(int[][] map, int N, int M) {
         int ey = N-1;
         int ex = M-1;
-        int[][][] visited = new int[2][N][M];
+        boolean[][][] visited = new boolean[2][N][M];
 
         ArrayDeque<Node> q = new ArrayDeque<>();
         q.offer(new Node(0, 0, 0, 1));
@@ -68,21 +61,19 @@ public class Main {
                 int ny = y + dy[d];
                 int nx = x + dx[d];
                 if (0 <= ny && ny < N && 0 <= nx && nx < M) {
-                    // z 가 0인 경우 돌파 가능
-                    if (z == 0) {
-                        if (map[0][ny][nx] == '1' && (visited[1][ny][nx] == 0 || dist + 1 < visited[1][ny][nx])) {
-                            visited[1][ny][nx] = dist + 1;
-                            q.offer(new Node(nx, ny, 1, dist + 1));
-                        }
+                    if (map[ny][nx] == '1' && z == 0 && !visited[1][ny][nx]) {
+                        visited[1][ny][nx] = true;
+                        q.offer(new Node(nx, ny, 1, dist + 1));
                     }
-                    if (map[z][ny][nx] == '0' && (visited[z][ny][nx] == 0 || dist + 1 < visited[z][ny][nx])) {
-                        visited[z][ny][nx] = dist + 1;
-                        q.offer(new Node(nx, ny, z, dist + 1));
+                    else {
+                        if (map[ny][nx] == '0' && !visited[z][ny][nx]) {
+                            visited[z][ny][nx] = true;
+                            q.offer(new Node(nx, ny, z, dist + 1));
+                        }
                     }
                 }
             }
         }
         return -1;
     }
-
 }
