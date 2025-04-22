37ab70b433b8f1262a067b56031da4522260e027
syann97 <shallom75321@gmail.com>
2025-04-22 13:25:15 +0900

    [Gold IV] Title: 빙산, Time: 368 ms, Memory: 45564 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -3,24 +3,12 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.*;
 
-class Node {
-    int x;
-    int y;
-
-    public Node (int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
 public class Main {
-    // static 변수 정의
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static int N;
     static int M;
     static int[][] area;
-    static boolean[][] isMelt;
     static boolean[][] visited;
     static int[] dy = {-1, 0, 1, 0};
     static int[] dx = {0, -1, 0, 1};
@@ -30,32 +18,27 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         area = new int[N][M];
-        isMelt = new boolean[N][M];
-
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 area[i][j] = Integer.parseInt(st.nextToken());
-                if (area[i][j] == 0) {
-                    isMelt[i][j] = true;
-                }
             }
         }
 
         int days = 0;
-        while (counting() <= 1) {
-            melt();
-            days++;
-            if (isAllMelted()) {
-                days = 0;
+        while (true) {
+            int cnt = countingDiv();
+            if (cnt == 1) days++;
+            else {
+                if (cnt == 0) days = 0;
                 break;
             }
         }
         System.out.println(days);
     }
 
-    static int counting() {
+    static int countingDiv() {
         int count = 0;
         visited = new boolean[N][M];
         for (int i = 0; i < N; i++) {
@@ -69,52 +52,24 @@ public class Main {
         return count;
     }
 
-    static void dfs(int y, int x) {
-        for (int d = 0; d < 4; d++) {
-            int ny = y + dy[d];
-            int nx = x + dx[d];
-            if (0 <= ny && ny < N && 0 <= nx && nx < M && !visited[ny][nx] && area[ny][nx] > 0) {
-                visited[ny][nx] = true;
-                dfs(ny, nx);
+    private static void dfs(int y, int x) {
+        visited[y][x] = true;
+
+        for(int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (0 <= ny && ny < N && 0 <= nx && nx < M && !visited[ny][nx]) {
+                if(area[ny][nx] == 0) {
+                    if(area[y][x] > 0) {
+                        area[y][x]--;
+                    }
+                }
+
+                if(area[ny][nx] > 0) {
+                    dfs(ny, nx);
+                }
             }
         }
     }
-
-    static void melt () {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                int cnt = calMelt(i, j);
-                if (cnt > area[i][j]) area[i][j] = 0;
-                else area[i][j] -= cnt;
-            }
-        }
-        // update
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (area[i][j] == 0) isMelt[i][j] = true;
-            }
-        }
-    }
-
-    static int calMelt(int y, int x) {
-        int count = 0;
-        for (int d = 0; d < 4; d++) {
-            int ny = y + dy[d];
-            int nx = x + dx[d];
-            if (0 <= ny && ny < N &&  0 <= nx && nx < M && isMelt[ny][nx]) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    static boolean isAllMelted() {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (area[i][j] != 0) return false;
-            }
-        }
-        return true;
-    }
-
 }
