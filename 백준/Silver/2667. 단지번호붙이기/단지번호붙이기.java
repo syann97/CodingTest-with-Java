8b19ecfe94a6b9e9586955f761d2dd8c495c1623
syann97 <shallom75321@gmail.com>
2025-04-29 09:26:43 +0900

    [Silver I] Title: 단지번호붙이기, Time: 76 ms, Memory: 11616 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());   // 도시 개수
+        char[][] map = new char[N][N];
+        boolean[][] visited = new boolean[N][N];
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = s.charAt(j);
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == '1' && !visited[i][j]) {
+                    answer.add(dfs(map, visited, i, j, 1));
+                }
+            }
+        }
+        System.out.println(answer.size());
+        Collections.sort(answer);
+        for (int num : answer) {
+            System.out.println(num);
+        }
+    }
+
+    static int dfs(char[][] map, boolean[][] visited, int y, int x, int count) {
+        visited[y][x] = true;
+        for (int d = 0; d < 4; d++) {
+            int ny = y + dy[d];
+            int nx = x + dx[d];
+
+            if (0 <= ny && ny < N && 0 <= nx && nx < N && map[ny][nx] == '1' && !visited[ny][nx]) {
+                count = dfs(map, visited, ny, nx, count + 1);
+            }
+        }
+        return count;
+    }
+}
