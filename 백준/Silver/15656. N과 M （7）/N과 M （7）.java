12eaceb5631bfa5919d125d8f08de53bc7d3fd5e
syann97 <shallom75321@gmail.com>
2025-05-04 00:29:31 +0900

    [Silver III] Title: N과 M (7), Time: 564 ms, Memory: 220484 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        int[] numbers = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numbers);
+        bt(numbers, new int[M], 0);
+        System.out.print(sb);
+    }
+
+    static void bt (int[] numbers, int[] memory, int count) {
+        if (count == M) {
+            for (int num : memory) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            memory[count] = numbers[i];
+            bt(numbers, memory, count + 1);
+        }
+    }
+}
