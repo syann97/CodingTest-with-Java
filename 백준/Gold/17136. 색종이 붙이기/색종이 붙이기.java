c1510382be8e44b7ab02b4e2cf6496b109d90e34
syann97 <shallom75321@gmail.com>
2025-05-15 09:42:44 +0900

    [Gold II] Title: 색종이 붙이기, Time: 212 ms, Memory: 21048 KB -BaekjoonHub

---
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringTokenizer st;
+    static int min = 26;
+    static int[] paperSize = {1, 2, 3, 4, 5};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[][] area = new int[10][10];
+
+        for (int i = 0; i < 10; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 10; j++) {
+                area[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        bfs(0, 0, area, new int[]{5, 5, 5, 5, 5}, 0);
+        System.out.println(min == 26 ? -1 : min);
+    }
+
+
+    static void bfs(int y, int x, int[][] area, int[] paperCount, int count) {
+        if (y == 10) {
+            min = Math.min(min, count);
+            return;
+        }
+
+        int ny = x + 1 == 10 ? y + 1 : y;
+        int nx = x + 1 == 10 ? 0 : x + 1;
+
+        if (area[y][x] == 1) {
+            int maxSize = checkPossibleSize(y, x, area);
+            for (int idx = 0; idx < 5; idx++) {
+                if (paperSize[idx] <= maxSize && paperCount[idx] > 0) {
+                    for (int row = y; row < y + paperSize[idx]; row++) {
+                        for (int col = x; col < x + paperSize[idx]; col++) {
+                            area[row][col] = 0;
+                        }
+                    }
+                    paperCount[idx]--;
+
+                    bfs(ny, nx, area, paperCount, count + 1); // deepcopy area
+
+                    for (int row = y; row < y + paperSize[idx]; row++) {
+                        for (int col = x; col < x + paperSize[idx]; col++) {
+                            area[row][col] = 1;
+                        }
+                    }
+                    paperCount[idx]++;
+                }
+            }
+        }
+        else {
+            bfs(ny, nx, area, paperCount, count);
+        }
+    }
+
+
+
+    private static int checkPossibleSize(int y, int x, int[][] area) {
+        for (int size = 5; size >= 1; size--) {
+            if (y + size > 10 || x + size > 10) continue;
+
+            boolean possible = true;
+            for (int i = y; i < y + size && possible; i++) {
+                for (int j = x; j < x + size; j++) {
+                    if (area[i][j] != 1) {
+                        possible = false;
+                        break;
+                    }
+                }
+            }
+
+            if (possible) return size;
+        }
+
+        return 0;
+    }
+}
