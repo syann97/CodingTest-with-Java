607d81ddc91cd1837ab12e99bf763bd40e625668
syann97 <shallom75321@gmail.com>
2025-05-18 01:16:06 +0900

    [Gold V] Title: 퇴사 2, Time: 604 ms, Memory: 302188 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -18,15 +18,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        Schedule[] schedules = new Schedule[N+1];
         int[] dp = new int[N+2];
 
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            schedules[i] = new Schedule(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-            if (i + schedules[i].T <= N+1) {
-                dp[i + schedules[i].T] = Math.max(dp[i + schedules[i].T], dp[i] + schedules[i].P);
-            }
+            int T = Integer.parseInt(st.nextToken());
+            int P = Integer.parseInt(st.nextToken());
+            
+            int next = i + T;
+            if (next <= N+1) dp[next] = Math.max(dp[next], dp[i] + P);
             dp[i+1] = Math.max(dp[i], dp[i+1]);
         }
         System.out.println(dp[N+1]);
