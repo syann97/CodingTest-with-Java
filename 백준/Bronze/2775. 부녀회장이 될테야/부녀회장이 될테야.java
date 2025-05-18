7f4a7e4df2a0d690db9719107e33d756180c8b3a
syann97 <shallom75321@gmail.com>
2025-05-18 14:14:55 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 64 ms, Memory: 11332 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -6,19 +6,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int K = Integer.parseInt(br.readLine());
+        int[][] dp = new int[15][15];
+        for (int col = 1; col <= 14; col++) dp[0][col] = col;
+
+        for (int row = 1; row <= 14; row++) {
+            for (int  col = 1; col <= 14; col++) {
+                dp[row][col] += dp[row-1][col] + dp[row][col-1];
+            }
+        }
 
         while (K --> 0) {
             int k = Integer.parseInt(br.readLine());
             int n = Integer.parseInt(br.readLine());
-            int[][] dp = new int[k + 1][n + 1];
-
-            for (int col = 1; col <= n; col++) dp[0][col] = col;
-
-            for (int row = 1; row <= k; row++) {
-                for (int  col = 1; col <= n; col++) {
-                    dp[row][col] += dp[row-1][col] + dp[row][col-1];
-                }
-            }
             System.out.println(dp[k][n]);
         }
     }
