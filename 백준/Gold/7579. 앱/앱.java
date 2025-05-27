56266e6ccce3bb3a7a036c34c35df396d5556fe1
syann97 <shallom75321@gmail.com>
2025-05-27 13:21:41 +0900

    [Gold III] Title: 앱, Time: 88 ms, Memory: 11924 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -1,14 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static final int INF = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
@@ -16,31 +14,33 @@ public class Main {
 
         int[] A = new int[N];
         int[] c = new int[N];
-        int[] dp = new int[M+1];
-
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(st.nextToken());
         }
 
+        int total_cost = 0;
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             c[i] = Integer.parseInt(st.nextToken());
+            total_cost += c[i];
         }
 
-        Arrays.fill(dp, INF);
-        dp[0] = 0;
+        int[] dp = new int[total_cost + 1];
+
 
         for (int i = 0; i < N; i++) {
-            int memory = A[i];
-            for (int j = M-1; j >= 0; j--) {
-                if (dp[j] != INF) {
-                    int tmp = Math.min((j + memory), M);
-                    dp[tmp] = Math.min(dp[tmp], dp[j] + c[i]);
-                }
+            for (int j = total_cost; j >= c[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j-c[i]] + A[i]);
             }
         }
-        System.out.println(dp[M]);
+
+        for (int i = 0; i <= total_cost; i++) {
+            if (dp[i] >= M) {
+                System.out.println(i);
+                break;
+            }
+        }
     }
 }
