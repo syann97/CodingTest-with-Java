046eafe011f5618e14d2c384dbe6cf298a081330
syann97 <shallom75321@gmail.com>
2025-05-29 17:15:08 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.96 ms, Memory: 56.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -4,35 +4,32 @@ class Node {
     int y;
     int x;
     int dist;
+    
     public Node(int y, int x, int dist) {
         this.y = y;
         this.x = x;
         this.dist = dist;
     }
 }
-
 class Solution {
-    static int[] dy = {0, -1, 0, 1};
-    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, -1, 0, 1};
     public int solution(int[][] maps) {
-
         return bfs(maps);
     }
     
     static int bfs(int[][] maps) {
         int N = maps.length;
         int M = maps[0].length;
-        
         int ey = N - 1;
         int ex = M - 1;
-        
-        boolean[][] visited = new boolean[N][M];
-        visited[0][0] = true;
         
         ArrayDeque<Node> q = new ArrayDeque<>();
         q.offer(new Node(0, 0, 1));
         
-            
+        boolean[][] visited = new boolean[N][M];
+        visited[0][0] = true;
+        
         while (!q.isEmpty()) {
             Node node = q.poll();
             int y = node.y;
@@ -52,6 +49,5 @@ class Solution {
             }
         }
         return -1;
-        
     }
 }
