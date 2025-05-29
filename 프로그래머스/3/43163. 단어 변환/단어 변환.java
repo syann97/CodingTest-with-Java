8a0f6704a700664feebe74c0a474670f360a8624
syann97 <shallom75321@gmail.com>
2025-05-29 18:09:51 +0900

    [level 3] Title: 단어 변환, Time: 1.06 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -3,21 +3,19 @@ import java.util.*;
 class Node {
     String s;
     int dist;
-    
-    public Node (String s, int dist) {
+    public Node(String s, int dist) {
         this.s = s;
         this.dist = dist;
     }
 }
 
-
 class Solution {
     public int solution(String begin, String target, String[] words) {
         ArrayDeque<Node> q = new ArrayDeque<>();
         q.offer(new Node(begin, 0));
+        
         Set<String> visited = new HashSet<>();
         visited.add(begin);
-        
         
         while (!q.isEmpty()) {
             Node node = q.poll();
@@ -36,14 +34,17 @@ class Solution {
                 }
             }
         }
+        
         return 0;
     }
     
     static boolean isConnect(String s1, String s2) {
         int count = 0;
+        
         for (int i = 0; i < s1.length(); i++) {
             if (s1.charAt(i) == s2.charAt(i)) count++;
         }
+        
         return count == s1.length() - 1;
     }
 }
