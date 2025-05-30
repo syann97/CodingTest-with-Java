8a159def62736f4daa42171032b63be6f227de27
syann97 <shallom75321@gmail.com>
2025-05-30 10:13:25 +0900

    [level 1] Title: 신규 아이디 추천, Time: 20.38 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,35 +1,35 @@
 class Solution {
     public String solution(String new_id) {
-        // 1단계: 대문자 -> 소문자 치환
+        // 1단계 소문자로 치환
         new_id = new_id.toLowerCase();
-        
-        // 2단계: 알파벳 소문자, 숫자, 빼기, 밑줄, 마침표 제외 제거
+        // 2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
         new_id = new_id.replaceAll("[^a-z0-9\\-_.]", "");
         
-        // 3단계: 마침표(.)가 2연속 이상인 것 하나로 치환
+        // 3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
         new_id = new_id.replaceAll("\\.{2,}", ".");
         
-        // 4단계: 마침표(.)가 시작이나 끝에 있다면 제거
+        // 4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
         new_id = new_id.replaceAll("^\\.|\\.$", "");
         
-        // 5단계: 빈 문자열 => a를 대입
+        // 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
         if (new_id.isEmpty()) new_id = "a";
         
-        // 6단계: 길이 16 이상 => 첫 15개 문자만 포함 + 제거 후 마침표(.)가 끝일 경우 제거
+        // 6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거
+        // 만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거
         if (new_id.length() >= 16) {
-            if (new_id.charAt(14) == '.') new_id = new_id.substring(0, 14);
+            if(new_id.charAt(14) == '.') new_id = new_id.substring(0, 14);
             else new_id = new_id.substring(0, 15);
         }
         
-        // 7단계: 길이가 2자 이하 => 마지막 문자를 길이 3이 될 때까지 반복 추가
+        
+        // 7단계 new_id의 길이가 2자 이하일 때, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 추가
         if (new_id.length() <= 2) {
-            char lastChar = new_id.charAt(new_id.length() - 1);
+            char c = new_id.charAt(new_id.length()-1);
             while (new_id.length() < 3) {
-                new_id += lastChar;
+                new_id += c;
             }
         }
-        System.out.println(new_id);
-
+        
         return new_id;
     }
 }
