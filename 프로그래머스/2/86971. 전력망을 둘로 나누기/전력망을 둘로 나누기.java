28e991d5eaeb67ad01e8d432cb773dbbe4cdbd8b
syann97 <shallom75321@gmail.com>
2025-05-30 09:37:44 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.27 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -3,46 +3,48 @@ import java.util.*;
 class Solution {
     static ArrayList<Integer>[] graph;
     public int solution(int n, int[][] wires) {
-        graph = new ArrayList[n+1];
         int answer = n;
+        graph = new ArrayList[n+1];
         
-        for (int i = 0; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             graph[i] = new ArrayList<>();
         }
         
-        // 인접 리스트 형태로 그래프 저장
+        // 인접 리스트 형태로 구현
         for (int i = 0; i < wires.length; i++) {
-            int v1 = wires[i][0];
-            int v2 = wires[i][1];
-            
-            graph[v1].add(v2);
-            graph[v2].add(v1);
-        }
+            graph[wires[i][0]].add(wires[i][1]);
+            graph[wires[i][1]].add(wires[i][0]);
+        } 
         
         
-        // flood fill 수행
+        
+       
+        // 각 전선 별 flood fill
         for (int i = 0; i < wires.length; i++) {
             int[] cut = wires[i];
-            boolean[] visited = new boolean[n+1];
             
-            int count = dfs(1, visited, cut);
+            // dfs 수행
+            int count = dfs(1, cut, new boolean[n+1]);
             int other = n - count;
+            // 송전탑 차이 계산
             answer = Math.min(answer, Math.abs(count - other));
         }
+        
+        
         return answer;
     }
     
-    
-    static int dfs(int v, boolean[] visited, int[] cut) {
+    static int dfs(int v, int[] cut, boolean[] visited) {
         visited[v] = true;
         
         int count = 1;
         for (int nv : graph[v]) {
             if ((v == cut[0] && nv == cut[1]) || (v == cut[1] && nv == cut[0])) continue;
             if (!visited[nv]) {
-                count += dfs(nv, visited, cut);
+                count += dfs(nv, cut, visited);
             }
         }
+        
         return count;
     }
 }
