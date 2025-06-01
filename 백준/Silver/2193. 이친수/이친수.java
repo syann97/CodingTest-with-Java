a72390798eef367f0c51fa993eeb4355100e2638
syann97 <shallom75321@gmail.com>
2025-06-01 13:45:41 +0900

    [Silver III] Title: 이친수, Time: 60 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if (N <= 2) {
+            System.out.println(1);
+        }
+        else {
+            long[] dp = new long[N+1];
+            dp[1] = 1;
+            dp[2] = 1;
+
+            for (int i = 3; i <= N; i++) {
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+
+            System.out.println(dp[N]);
+        }
+    }
+}
