1d9b43fa5f4dd77e45c92da304ad3712ddb06fc0
syann97 <shallom75321@gmail.com>
2025-06-02 13:39:48 +0900

    [Gold IV] Title: 로또, Time: 72 ms, Memory: 12248 KB -BaekjoonHub

--- 백준/Gold/2758. 로또/로또.java
+++ 백준/Gold/2758. 로또/로또.java
@@ -1,20 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static StringTokenizer st;
     static StringBuilder sb;
-    static int N = 10;
-    static int M = 2000;
+    static final int N = 10;
+    static final int M = 2000;
+    static long[][] dp = new long[N+1][M+1];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
-        long[][] dp = new long[N+1][M+1];
-        for (int i = 1; i <= M; i++) {
-            dp[1][i] = i;
-        }
+
+        for (int i = 1; i <= M; i++) dp[1][i] = i;
 
         for (int i = 2; i <= N; i++) {
             for (int j = (int)Math.pow(2, i-1); j <= M; j++) {
@@ -27,7 +27,6 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
-
             sb.append(dp[n][m]).append("\n");
         }
         System.out.println(sb);
