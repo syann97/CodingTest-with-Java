3c1238c421f56479f4971032e27d95fd3c828127
syann97 <shallom75321@gmail.com>
2025-06-02 11:38:09 +0900

    [Gold IV] Title: 로또, Time: 76 ms, Memory: 12244 KB -BaekjoonHub

--- 백준/Gold/2758. 로또/로또.java
+++ 백준/Gold/2758. 로또/로또.java
@@ -1,13 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static StringTokenizer st;
+    static StringBuilder sb;
+    static int N = 10;
+    static int M = 2000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        long[][] dp = new long[N+1][M+1];
+        for (int i = 1; i <= M; i++) {
+            dp[1][i] = i;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = (int)Math.pow(2, i-1); j <= M; j++) {
+                dp[i][j] = dp[i-1][j/2] + dp[i][j-1];
+            }
+        }
 
         int T = Integer.parseInt(br.readLine());
         while (T --> 0) {
@@ -15,28 +28,8 @@ public class Main {
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
 
-            if (Math.pow(2, n-1) > m) System.out.println(0);
-            else {
-                long[][] dp = new long[n][m+1];
-                Arrays.fill(dp[0], 1);
-
-
-                for (int i = 1; i < n; i++) {
-                    for (int j = 1; j <= m; j++) {
-                        if (dp[i-1][j] != 0) {
-                            for (int k = j * 2; k <= m; k++) {
-                                dp[i][k] += dp[i-1][j];
-                            }
-                        }
-                    }
-                }
-
-                long answer = 0;
-                for (int j = (int)Math.pow(2, n-1); j <=m; j++) {
-                    answer += dp[n-1][j];
-                }
-                System.out.println(answer);
-            }
+            sb.append(dp[n][m]).append("\n");
         }
+        System.out.println(sb);
     }
 }
