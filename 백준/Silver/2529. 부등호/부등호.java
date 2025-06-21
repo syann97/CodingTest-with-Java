a0b617ea7d0f78154036a8c315d2500fd91af294
syann97 <shallom75321@gmail.com>
2025-06-21 17:56:01 +0900

    [Silver I] Title: 부등호, Time: 64 ms, Memory: 11676 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -1,12 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
-    static StringTokenizer st;
-    static int answer = 0;
-    static boolean flag;
     static StringBuilder sb;
 
     public static void main(String[] args) throws Exception {
@@ -18,37 +13,35 @@ public class Main {
 
         char[] operations = s.replace(" ", "").toCharArray();
         int[] answer = new int[N+1];
+        boolean[] visited;
 
-        flag = false;
         for (int i = 9; i >= 0; i--) {
-            if (flag) break;
-            answer[0] = i;
-            boolean[] visited = new boolean[10];
+            visited = new boolean[10];
             visited[i] = true;
-            bt(0, N, true, operations, answer, visited);
+            answer[0] = i;
+
+            if(bt(0, N, true, operations, answer, visited)) break;
         }
         sb.append("\n");
 
-        flag = false;
         for (int i = 0; i <= 9; i++) {
-            if (flag) break;
-            answer[0] = i;
-            boolean[] visited = new boolean[10];
+            visited = new boolean[10];
             visited[i] = true;
-            bt(0, N, false, operations, answer, visited);
+            answer[0] = i;
+
+            if (bt(0, N, false, operations, answer, visited)) break;
         }
 
         System.out.println(sb);
     }
 
 
-    static void bt(int idx, int N, boolean isReverse, char[] operations, int[] answer, boolean[] visited) {
+    static boolean bt(int idx, int N, boolean isReverse, char[] operations, int[] answer, boolean[] visited) {
         if (idx == N) {
             for (int i = 0; i <= N; i++) {
                 sb.append(answer[i]);
             }
-            flag = true;
-            return;
+            return true;
         }
 
         int target = answer[idx];
@@ -56,10 +49,9 @@ public class Main {
             for (int num = 0; num <= 9; num++) {
                 if (!visited[num]) {
                     if ((operations[idx] == '<' && target < num) || (operations[idx] == '>' && target > num)) {
-                        if (flag) return;
                         answer[idx+1] = num;
                         visited[num] = true;
-                        bt(idx+1, N, isReverse, operations, answer, visited);
+                        if (bt(idx+1, N, isReverse, operations, answer, visited)) return true;
                         visited[num] = false;
                     }
                 }
@@ -69,14 +61,14 @@ public class Main {
             for (int num = 9; num >= 0; num--) {
                 if (!visited[num]) {
                     if ((operations[idx] == '<' && target < num) || (operations[idx] == '>' && target > num)) {
-                        if (flag) return;
                         answer[idx+1] = num;
                         visited[num] = true;
-                        bt(idx+1, N, isReverse, operations, answer, visited);
+                        if (bt(idx+1, N, isReverse, operations, answer, visited)) return true;
                         visited[num] = false;
                     }
                 }
             }
         }
+        return false;
     }
 }
