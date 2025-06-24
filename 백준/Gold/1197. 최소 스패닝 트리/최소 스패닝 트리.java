663a7bf364469d7e1334f688cf945edce0fa1d0d
syann97 <shallom75321@gmail.com>
2025-06-24 13:42:26 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 448 ms, Memory: 50348 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -67,9 +67,11 @@ public class Main {
     static void union(int a, int b) {
         a = find(a);
         b = find(b);
+
         
-        if (a > b) parent[a] = b;
-        else parent[b] = a;
+        if (a != b) {
+            parent[b] = a;
+        }
     }
 
     static int find(int a) {
