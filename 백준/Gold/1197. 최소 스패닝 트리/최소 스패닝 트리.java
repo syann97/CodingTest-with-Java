b6a2a2ebcbbdafc05adc9d507d379f16b63bed02
syann97 <shallom75321@gmail.com>
2025-06-24 16:12:11 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 240 ms, Memory: 20788 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,7 +1,38 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.util.PriorityQueue;
+
+class Reader {
+    private final int SIZE = 1 << 13;
+    private byte[] buffer = new byte[SIZE];
+    private int size, index;
+
+    private byte read() throws IOException {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+
+    int nextInt() throws IOException {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+
+        while ((c = read()) <= 32);
+
+        if (c == 45) {
+            isMinus = true;
+            c = read();
+        }
+
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (48 <= (c = read()) && c <= 57);
+
+        return isMinus ? ~n + 1 : n;
+    }
+}
 
 
 class Edge implements Comparable<Edge> {
@@ -26,24 +57,21 @@ public class Main {
     static int[] parent;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
-
         edges = new PriorityQueue<>();
+        Reader in = new Reader();
+
+        int V = in.nextInt();
+        int E = in.nextInt();
+
         parent = new int[V + 1];
         for (int i = 1; i <= V; i++) {
             parent[i] = i;
         }
 
         for (int i = 1; i <= E; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
+            int u = in.nextInt();
+            int v = in.nextInt();
+            int w = in.nextInt();
 
             edges.add(new Edge(u, v, w));
         }
@@ -66,7 +94,6 @@ public class Main {
     static boolean union(int a, int b) {
         a = find(a);
         b = find(b);
-
 
         if (a == b) return false;
         parent[b] = a;
