49ae95ed893d7b2109950d9d8a7539bc129bf51c
syann97 <shallom75321@gmail.com>
2025-06-24 15:30:28 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 488 ms, Memory: 53612 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -1,7 +1,44 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.util.PriorityQueue;
+
+class Reader {
+    private final int SIZE = 1 << 13;
+    private byte[] buffer = new byte[SIZE];
+    private int index, size;
+
+    private byte read() throws IOException {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+
+    private boolean isNumber(byte c) {
+        return 48 <= c && c <= 57;
+    }
+
+    int nextInt() throws IOException {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+
+        while ((c = read()) <= 32) {
+            if (size < 0) return -1;
+        }
+
+        if (c == 45) {
+            isMinus = true;
+            c = read();
+        }
+
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+
+        return isMinus ? ~n + 1 : n;
+    }
+}
 
 
 class Edge implements Comparable<Edge> {
@@ -26,24 +63,21 @@ public class Main {
     static int[] parent;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
-
         edges = new PriorityQueue<>();
+        Reader in = new Reader();
+
+        int V = in.nextInt();
+        int E = in.nextInt();
+
         parent = new int[V + 1];
         for (int i = 1; i <= V; i++) {
             parent[i] = i;
         }
 
         for (int i = 1; i <= E; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
+            int u = in.nextInt();
+            int v = in.nextInt();
+            int w = in.nextInt();
 
             edges.add(new Edge(u, v, w));
         }
@@ -68,8 +102,7 @@ public class Main {
     static boolean union(int a, int b) {
         a = find(a);
         b = find(b);
-
-
+        
         if (a == b) return false;
         parent[b] = a;
         return true;
