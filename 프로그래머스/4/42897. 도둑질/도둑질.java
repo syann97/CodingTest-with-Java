05844652363d2141ba105ae0e02157761fd86d3f
syann97 <shallom75321@gmail.com>
2025-07-02 17:47:17 +0900

    [level 4] Title: 도둑질, Time: 32.43 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,27 +1,18 @@
-import java.util.*;
-
 class Solution {
-    public int solution(int[] nums) {
-        int n = nums.length;
-
-        int max1 = robLinear(Arrays.copyOfRange(nums, 0, n - 1));
-        int max2 = robLinear(Arrays.copyOfRange(nums, 1, n));
-
-        return Math.max(max1, max2);
-    }
-
-    private int robLinear(int[] nums) {
-        int n = nums.length;
+    public int solution(int[] money) {
+        int n = money.length;
+        int[][] dp = new int[2][n];
         
-        int[] dp = new int[n];
+        dp[1][0] = 0;
+        dp[0][0] = money[0];
+        dp[0][1] = money[0];
+        dp[1][1] = money[1];
         
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-
         for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+            dp[0][i] = Math.max(dp[0][i-1], dp[0][i-2] + money[i]);
+            dp[1][i] = Math.max(dp[1][i-1], dp[1][i-2] + money[i]);
         }
-
-        return dp[n - 1];
+        
+        return Math.max(dp[0][n-2], dp[1][n-1]);
     }
 }
