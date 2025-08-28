88f9cd0133fdf11b76f2dd8577c9196dac131d6b
syann97 <shallom75321@gmail.com>
2025-08-28 23:41:20 +0900

    [Bronze IV] Title: 모범생 포닉스, Time: 84 ms, Memory: 11580 KB -BaekjoonHub

---
+++ 백준/Bronze/28097. 모범생 포닉스/모범생 포닉스.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int sum = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i < N; i++) {
+            sum += Integer.parseInt(st.nextToken()) + 8;
+        }
+
+        System.out.println(sum / 24 + " " + sum % 24);
+    }
+}
