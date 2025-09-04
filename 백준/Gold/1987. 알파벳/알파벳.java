e0b6a52dedef945e27a178c45d6663a2216e2c88
syann97 <shallom75321@gmail.com>
2025-09-04 13:27:04 +0900

    [Gold IV] Title: 알파벳, Time: 824 ms, Memory: 12048 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -15,17 +15,20 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        char[][] board = new char[N][M];
-        for (int i = 0; i < N; i++) {
-            board[i] = br.readLine().toCharArray();
+        int[][] board = new int[N][M];
+        for (int row = 0; row < N; row++) {
+            char[] tmp = br.readLine().toCharArray();
+            for (int col = 0; col < M; col++) {
+                board[row][col] = tmp[col] - 65;
+            }
         }
 
-        dfs(1, 0, 0, board, 1 << (board[0][0] - 65));
+        dfs(1, 0, 0, board, 1 << board[0][0]);
 
         System.out.println(max);
     }
 
-    static void dfs(int count, int y, int x, char[][] board, int alphabetMasking) {
+    static void dfs(int count, int y, int x, int[][] board, int alphabetMasking) {
         max = Math.max(max, count);
 
         if (max == 26) return;
@@ -36,7 +39,7 @@ public class Main {
 
             if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
 
-            int currentAlphabet = 1 << (board[ny][nx] - 'A');
+            int currentAlphabet = 1 << (board[ny][nx]);
             if ((alphabetMasking & currentAlphabet) == 0) {
                 dfs(count + 1, ny, nx, board, alphabetMasking | currentAlphabet);
             }
