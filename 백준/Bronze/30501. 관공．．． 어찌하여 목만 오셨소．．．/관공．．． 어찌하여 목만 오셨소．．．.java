a694bb716d1764385d1e6e39062f08c9173f6ebf
syann97 <shallom75321@gmail.com>
2025-09-06 23:49:22 +0900

    [Bronze IV] Title: 관공... 어찌하여 목만 오셨소..., Time: 68 ms, Memory: 11748 KB -BaekjoonHub

---
+++ 백준/Bronze/30501. 관공．．． 어찌하여 목만 오셨소．．．/관공．．． 어찌하여 목만 오셨소．．．.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            if (s.contains("S")) {
+                System.out.println(s);
+                break;
+            }
+        }
+    }
+}
