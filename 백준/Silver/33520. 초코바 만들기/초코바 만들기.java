259607d428813c8b5bfc36d71f0b15666c78feea
syann97 <shallom75321@gmail.com>
2025-09-14 17:43:04 +0900

    [Silver V] Title: 초코바 만들기, Time: 364 ms, Memory: 67860 KB -BaekjoonHub

---
+++ 백준/Silver/33520. 초코바 만들기/초코바 만들기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int min = 0;
+        int max = 0;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            min = Math.max(min, Math.min(a, b));
+            max = Math.max(max, Math.max(a, b));
+        }
+        System.out.println((long) min * max);
+    }
+}
