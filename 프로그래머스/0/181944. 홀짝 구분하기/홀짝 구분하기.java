e1376d21c9738a7e79166217e847314ddf273979
syann97 <shallom75321@gmail.com>
2025-09-28 17:04:33 +0900

    [level 0] Title: 홀짝 구분하기, Time: 155.21 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n + " is " + (n % 2 == 0 ? "even" : "odd"));
+    }
+}
