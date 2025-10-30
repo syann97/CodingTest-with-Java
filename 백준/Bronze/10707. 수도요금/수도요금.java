ab884366c76ef543cc778aaf30f9260246a43f47
syann97 <shallom75321@gmail.com>
2025-10-30 23:00:29 +0900

    [Bronze III] Title: 수도요금, Time: 60 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Bronze/10707. 수도요금/수도요금.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int D = Integer.parseInt(br.readLine());
+        int P = Integer.parseInt(br.readLine());
+
+        int X = P * A;
+        int Y = C >= P ? B : B + ((P - C) * D);
+
+        System.out.println(Math.min(X, Y));
+    }
+}
