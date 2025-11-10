879e44ce26e85080857c7577559700781afad50a
syann97 <shallom75321@gmail.com>
2025-11-10 20:47:57 +0900

    [Bronze III] Title: 학술대회 참가신청, Time: 64 ms, Memory: 11460 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/32969. 학술대회 참가신청/학술대회 참가신청.java
+++ 백준/Bronze/32969. 학술대회 참가신청/학술대회 참가신청.java
@@ -10,25 +10,29 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] words = br.readLine().split(" ");
 
+        int dhCount = 0;
+        int bdCount = 0;
         boolean flag = false;
         for (String word : words) {
             for (String target : dh) {
                 if (word.equals(target)) {
-                    System.out.println("digital humanities");
                     flag = true;
+                    dhCount++;
                     break;
                 }
             }
 
             for (String target : bd) {
                 if (word.equals(target)) {
-                    System.out.println("public bigdata");
                     flag = true;
+                    bdCount++;
                     break;
                 }
             }
 
             if (flag) break;
         }
+
+        System.out.println(dhCount >= bdCount ? "digital humanities" : "public bigdata");
     }
 }
