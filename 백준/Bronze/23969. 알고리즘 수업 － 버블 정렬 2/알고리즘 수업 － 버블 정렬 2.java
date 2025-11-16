db177610e1f567d8e33e2191aeefe4318933babd
syann97 <shallom75321@gmail.com>
2025-11-16 18:00:11 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 2, Time: 320 ms, Memory: 15932 KB -BaekjoonHub

---
+++ 백준/Bronze/23969. 알고리즘 수업 － 버블 정렬 2/알고리즘 수업 － 버블 정렬 2.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+class Main {
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i  = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int count = 0;
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - 1 - i; j++) {
+                if (count == K) break;
+                if (arr[j] > arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                    count++;
+                }
+            }
+        }
+
+        if (count == K) {
+            for (int i = 0; i < N; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+        }
+        else sb.append("-1");
+
+        System.out.println(sb);
+
+    }
+}
