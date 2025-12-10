435574615703d7c6fb49f833b9876de86b39e8ed
syann97 <shallom75321@gmail.com>
2025-12-10 22:31:15 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 68 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.java
+++ 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.java
@@ -1,10 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 
 public class Main {
-    static HashMap<String, String> map = new HashMap<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
