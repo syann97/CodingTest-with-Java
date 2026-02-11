238f55505aa5ade395a66b6cf0869abde5faa647
syann97 <shallom75321@gmail.com>
2026-02-11 14:57:29 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 31.19 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -22,30 +22,13 @@ class Solution {
         long total = 0;
         int time_prev = 0;
         
-        // System.out.println("level : " + level);
-        
         for (int i = 0; i < diffs.length; i++) {
             int time_cur = times[i];
             int plus = (time_cur + time_prev) * (Math.max(diffs[i] - level, 0)) + time_cur;
-            // System.out.println((i+1) + "번째 퍼즐");
-            // System.out.println("cur : " + time_cur + "  plus : " + plus);
-            // System.out.println();
             total += plus;
             time_prev = time_cur;
         }
                
         return total <= limit;
     }
-    
 }
-
-
-// diff [1, 99999, 100000, 99995]	times [9999, 9001, 9999, 9001]
-// 공식 (times[i] + time_prev) * (diff[i] - 숙련도(level)) + 9001;
-
-// diff - level = 틀린 횟수
-// time_cur + time_prev = 이전 퍼즐 다시 풀고오는 시간
-// 즉 prev에 이미 푼 건 누적해야됨
-
-// 숙련도의 최솟값 구하기
-// 이분 탐색 
