ccc7235da10617200dbfb59e4b55f40e96be5343
syann97 <shallom75321@gmail.com>
2026-02-13 17:49:34 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 201.28 ms, Memory: 201 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -4,33 +4,35 @@ import java.util.*;
 class Solution {
     public int[] solution(int[][] edges) {
         
-        int[][] degrees = new int[2][1000001];
+        int[][] degrees = new int[1000001][2];
         boolean[] visited = new boolean[1000001];
         
         
         for (int[] edge : edges) {
-            int out = edge[0];
-            int in = edge[1];
-            
-            if (!visited[out]) visited[out] = true;
-            if (!visited[in]) visited[in] = true;
-            
-            degrees[1][out]++;
-            degrees[0][in]++;
+            int u = edge[0];
+            int v = edge[1];
+
+            visited[u] = true;
+            visited[v] = true;
+
+            degrees[u][1]++;
+            degrees[v][0]++;
         }
-        
         
         
         int[] answer = new int[4];
         for (int u = 1; u <= 1000000; u++) {
             if (!visited[u]) continue;
             
-            if (degrees[0][u] == 0 && degrees[1][u] >= 2) answer[0] = u;
-            else if (degrees[1][u] == 0) answer[2]++;
-            else if (degrees[1][u] == 2) answer[3]++;
+            int in = degrees[u][0];
+            int out = degrees[u][1];
+
+            if (in == 0 && out >= 2) answer[0] = u;
+            else if (out == 0) answer[2]++;
+            else if (out == 2) answer[3]++;
         }
         
-        answer[1] = degrees[1][answer[0]] - answer[2] - answer[3];
+        answer[1] = degrees[answer[0]][1] - answer[2] - answer[3];
         
         return answer;
     }
