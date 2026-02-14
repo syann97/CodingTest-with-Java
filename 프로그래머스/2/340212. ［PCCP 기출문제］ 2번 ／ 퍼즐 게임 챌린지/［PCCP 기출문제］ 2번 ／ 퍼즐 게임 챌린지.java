54dabdada55b59e9ebc107d63a11f3a2f981ce78
syann97 <shallom75321@gmail.com>
2026-02-15 01:46:53 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 45.22 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,34 +1,40 @@
 class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
-        int end = 0;
+        int s = 1;
+        int e = 0;
         
         for (int diff : diffs) {
-            end = Math.max(end, diff);
+            e = Math.max(e, diff);
         }
         
-        return div(1, end, diffs, times, limit);
+        int m;
+        int level;
+        
+        while (s < e) {
+            m = (s + e) / 2;
+            
+            if (calculate(m, diffs, times, limit) <= limit) {
+                e = m;
+            }
+            else {
+                s = m + 1;
+            }
+        }
+        
+        return e;
     }
     
-    static int div(int s, int e, int[] diffs, int[] times, long limit) {
-        if (s >= e) return e;
+    static long calculate (int level, int[] diffs, int[] times, long limit) {
+        long total = times[0];
+        int time_prev = times[0];
         
-        int m = (s + e) / 2;
-        
-        if (isPossible(diffs, times, limit, m)) return div(s, m, diffs, times, limit);
-        else return div(m+1, e, diffs, times, limit);
-    }
-    
-    static boolean isPossible (int[] diffs, int[] times, long limit, int level) {
-        long total = 0;
-        int time_prev = 0;
-        
-        for (int i = 0; i < diffs.length; i++) {
+        for (int i = 1; i < diffs.length; i++) {
             int time_cur = times[i];
-            int plus = (time_cur + time_prev) * (Math.max(diffs[i] - level, 0)) + time_cur;
-            total += plus;
+            total += (time_prev + time_cur) * (Math.max(diffs[i] - level, 0)) + time_cur;
             time_prev = time_cur;
         }
-               
-        return total <= limit;
+        
+        
+        return total;
     }
 }
