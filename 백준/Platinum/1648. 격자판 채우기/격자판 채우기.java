aac09928971ccba25c4943aa8a3c3cad5c007e26
syann97 <shallom75321@gmail.com>
2026-02-23 12:33:08 +0900

    [Platinum III] Title: 격자판 채우기, Time: 104 ms, Memory: 12944 KB -BaekjoonHub

--- 백준/Platinum/1648. 격자판 채우기/격자판 채우기.java
+++ 백준/Platinum/1648. 격자판 채우기/격자판 채우기.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,26 +12,34 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] dp = new int[N*M][1<<M];
+        int[][] dp = new int[2][1<<M];
         dp[0][0] = 1;
 
+        int before = 0;
+        int current = 1;
         for (int index = 0; index < N*M-1; index++) {
-            for (int state = 0; state < 1 << M; state++) {
-                if (dp[index][state] == 0) continue;
+            Arrays.fill(dp[current], 0);
+
+            for (int state = (1<<M) - 1; state >= 0; state--) {
+                if (dp[before][state] == 0) continue;
+
                 if ((state & 1) == 1) {
-                    dp[index+1][state>>1] = (dp[index+1][state>>1] + dp[index][state]) % 9901;
+                    dp[current][state>>1] = (dp[current][state>>1] + dp[before][state]) % 9901;
                 }
                 else {
                     if ((index % M != M-1) && (state & 2) == 0) {
-                        dp[index+1][(state>>1)|1] = (dp[index+1][(state>>1)|1] + dp[index][state]) % 9901;
+                        dp[current][(state>>1)|1] = (dp[current][(state>>1)|1] + dp[before][state]) % 9901;
                     }
 
                     if (index+M < M*N) {
-                        dp[index+1][(state>>1)|(1<<(M-1))] = (dp[index][state] + dp[index+1][(state>>1)|(1<<(M-1))]) % 9901;
+                        dp[current][(state>>1)|(1<<(M-1))] = (dp[current][(state>>1)|(1<<(M-1))] + dp[before][state]) % 9901;
                     }
                 }
             }
+            before ^= 1;
+            current ^= 1;
         }
-        System.out.println(dp[N*M-1][1]);
+
+        System.out.println(dp[before][1]);
     }
 }
