a92a6dbca78b7abe17334a97f9617ace4f9292d7
syann97 <shallom75321@gmail.com>
2026-02-23 12:09:58 +0900

    [Platinum III] Title: 격자판 채우기, Time: 132 ms, Memory: 24556 KB -BaekjoonHub

--- 백준/Platinum/1648. 격자판 채우기/격자판 채우기.java
+++ 백준/Platinum/1648. 격자판 채우기/격자판 채우기.java
@@ -3,37 +3,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Main {
-    static StringTokenizer st;
-
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] dp = new int[N*M+1][1<<M];
+        int[][] dp = new int[N*M][1<<M];
         dp[0][0] = 1;
 
-        for (int index = 0; index < N*M; index++) {
-            for (int state = 0; state < Math.pow(2, M); state++) {
+        for (int index = 0; index < N*M-1; index++) {
+            for (int state = 0; state < 1 << M; state++) {
                 if (dp[index][state] == 0) continue;
-
                 if ((state & 1) == 1) {
-                    dp[index+1][state>>1] = (dp[index][state] + dp[index+1][state>>1]) % 9901;
+                    dp[index+1][state>>1] = (dp[index+1][state>>1] + dp[index][state]) % 9901;
                 }
                 else {
                     if ((index % M != M-1) && (state & 2) == 0) {
-                        dp[index+1][(state>>1)|1] = (dp[index][state] + dp[index+1][(state>>1)|1]) % 9901;
+                        dp[index+1][(state>>1)|1] = (dp[index+1][(state>>1)|1] + dp[index][state]) % 9901;
                     }
 
-                    if (index + M < N * M) {
+                    if (index+M < M*N) {
                         dp[index+1][(state>>1)|(1<<(M-1))] = (dp[index][state] + dp[index+1][(state>>1)|(1<<(M-1))]) % 9901;
                     }
                 }
             }
         }
-
-        System.out.println(dp[N*M][0] % 9901);
+        System.out.println(dp[N*M-1][1]);
     }
 }
