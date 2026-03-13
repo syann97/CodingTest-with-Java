15431661527dffae254d1c9eb411af25f4029854
syann97 <shallom75321@gmail.com>
2026-03-13 19:18:31 +0900

    [Bronze II] Title: Tautogram, Time: 112 ms, Memory: 16652 KB -BaekjoonHub

---
+++ 백준/Bronze/5698. Tautogram/Tautogram.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            char baseChar = Character.toUpperCase(st.nextToken().charAt(0));
+
+            if (baseChar == '*') break;
+
+            boolean isTautogram = true;
+            while (st.hasMoreTokens()) {
+                if (baseChar != Character.toUpperCase(Character.toUpperCase(st.nextToken().charAt(0)))) {
+                    isTautogram = false;
+                    break;
+                }
+            }
+            sb.append(isTautogram ? "Y" : "N").append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
