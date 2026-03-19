d524ea13b646c43b6f778dc3338f416f0eb34a5d
syann97 <shallom75321@gmail.com>
2026-03-19 13:21:15 +0900

    [Gold III] Title: 경사로, Time: 88 ms, Memory: 12896 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.java
+++ 백준/Gold/14890. 경사로/경사로.java
@@ -3,22 +3,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
-class Node {
-    int y;
-    int x;
-
-    public Node (int y, int x) {
-        this.y = y;
-        this.x = x;
-    }
-}
-
 public class Main {
     static StringTokenizer st;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int L = Integer.parseInt(st.nextToken());
         int[][] map = new int[N][N];
@@ -30,57 +21,61 @@ public class Main {
             }
         }
 
-
         int count = 0;
 
         for (int i = 0; i < N; i++) {
-            count += getAnswer(map, i, N, L, true);
-            count += getAnswer(map, i, N, L, false);
+            if (canBuildRoad(map, i, N, L, true)) count++;
+            if (canBuildRoad(map, i, N, L, false)) count++;
         }
 
         System.out.println(count);
     }
 
-    static int getAnswer(int[][] map, int pos, int N, int L, boolean isRow) {
-        int[] right = new int[N];
-
-        right[N-1] = 1;
-
+    static boolean canBuildRoad(int[][] map, int pos, int N, int L, boolean isRow) {
+        int[] suffixSameCount = new int[N];
+        suffixSameCount[N - 1] = 1;
 
         int current = isRow ? map[pos][N-1] : map[N-1][pos];
-        for (int i = N-2; i >= 0; i--) {
-            int next = isRow ? map[pos][i] : map[i][pos];
-            if (current != next) {
-                current = next;
-                right[i] = 1;
-            }
-            else {
-                right[i] = right[i+1] + 1;
+        for (int i = N - 2; i >= 0; i--) {
+            int nextHeight = isRow ? map[pos][i] : map[i][pos];
+            if (current != nextHeight) {
+                current = nextHeight;
+                suffixSameCount[i] = 1;
+            } else {
+                suffixSameCount[i] = suffixSameCount[i + 1] + 1;
             }
         }
 
         current = isRow ? map[pos][0] : map[0][pos];
-        int stack = 1;
+        int count = 1;
 
         for (int i = 1; i < N; i++) {
             int next = isRow ? map[pos][i] : map[i][pos];
+
             if (next == current) {
-                stack++;
+                count++;
             }
             else if (next == current - 1) {
-                if (right[i] >= L) {
+                if (suffixSameCount[i] >= L) {
                     current = next;
-                    stack = -L+1;
+                    count = -L + 1;
+                } else {
+                    return false;
                 }
-                else return 0;
             }
-            else if (next == current + 1 && stack >= L) {
-                current = next;
-                stack = 1;
+            else if (next == current + 1) {
+                if (count >= L) {
+                    current = next;
+                    count = 1;
+                } else {
+                    return false;
+                }
             }
-            else return 0;
+            else {
+                return false;
+            }
         }
 
-        return 1;
+        return true;
     }
 }
