c792918c27cd0bcc801a408148c6bdb51748ef17
syann97 <shallom75321@gmail.com>
2026-03-20 12:05:05 +0900

    [Gold III] Title: 나무 재테크, Time: 516 ms, Memory: 218964 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.java
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 
 class Wood implements Comparable<Wood> {
@@ -24,7 +23,7 @@ class Wood implements Comparable<Wood> {
 
 public class Main {
     static StringTokenizer st;
-    static PriorityQueue<Wood> diedWood;
+    static Deque<Wood> diedWood;
     static int[] dy = {-1, -1, -1, 0, 0, 1, 1, 1};
     static int[] dx = {-1, 0, 1, -1, 1, -1, 0, 1};
 
@@ -47,28 +46,35 @@ public class Main {
             }
         }
 
-        PriorityQueue<Wood> pq = new PriorityQueue<>();
-        for (int i = 0; i < M; i++) {
+        ArrayList<Wood> woodList = new ArrayList<>();
 
-            // 주의 (x, y) 임
+        for (int i = 0; i < M; i++) {
+            // 주의 (x, y)
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             int z = Integer.parseInt(st.nextToken());
 
             // (x, y)의 나무 나이 z
-            pq.offer(new Wood(x-1, y-1, z));
+            woodList.add(new Wood(x-1, y-1, z));
         }
 
-        diedWood = new PriorityQueue<>();
+        Collections.sort(woodList);
+
+        diedWood = new ArrayDeque<>();
+        ArrayDeque<Wood> woods = new ArrayDeque<>();
+
+        woods.addAll(woodList);
+        woodList = null;
+
         while(K-- > 0) {
-            pq = spring(area, pq);
+            spring(area, woods);
             summer(area);
-            fall(pq, N);
+            fall(woods, N);
             winter(A, area, N);
         }
 
-        System.out.println(pq.size());
+        System.out.println(woods.size());
     }
 
     // 겨울: 로봇이 돌아다니며 양분 추가, A[r][c] 만큼씩
@@ -82,59 +88,51 @@ public class Main {
 
     // 가을: 나무 번식 (5의 배수) -> 인접 8칸 나이1 나무 생성
     // *주의* 같은 위치에 여러 나무 가능
-    static void fall(PriorityQueue<Wood> pq, int N) {
-        int ny, nx;
-        PriorityQueue<Wood> tmp = new PriorityQueue<>();
-        for (Wood wood : pq) {
+    static void fall(ArrayDeque<Wood> woods, int N) {
+        ArrayDeque<Wood> tmp = new ArrayDeque<>();
+        for (Wood wood : woods) {
             if (wood.z % 5 == 0) {
                 for (int d = 0; d < 8; d++) {
-                    nx = wood.x + dx[d];
-                    ny = wood.y + dy[d];
+                    int nx = wood.x + dx[d];
+                    int ny = wood.y + dy[d];
 
                     if (0 <= nx && nx < N && 0 <= ny && ny < N) {
-                        tmp.offer(new Wood(nx, ny, 1));
+                        tmp.add(new Wood(nx, ny, 1));
                     }
                 }
             }
         }
 
         for (Wood wood : tmp) {
-            pq.offer(wood);
+            woods.addFirst(wood);
         }
     }
 
     // 여름: 봄에 죽은 나무가 양분으로 변환 (나이 / 2, 소수점 X) (v)
     static void summer(int[][] area) {
-        Wood wood;
-
         while (!diedWood.isEmpty()) {
-            wood = diedWood.poll();
+            Wood wood = diedWood.poll();
             area[wood.x][wood.y] += wood.z / 2;
         }
     }
 
 
-
     // 봄: 나무는 자신의 나이만큼 양분 먹고(v), 나이 1 증가(v)
     // *주의* : 하나의 칸에 여러 개의 나무가 존재시 어린 나무부터 양분을 먹음(v)
     // *주의* : 죽은나무는 여름에 사용되어야 함
-    static PriorityQueue<Wood> spring(int[][] area, PriorityQueue<Wood> pq) {
-        Wood wood;
-
-        PriorityQueue<Wood> tmp = new PriorityQueue<>();
-
-        while (!pq.isEmpty()) {
-            wood = pq.poll();
+    static void spring(int[][] area, Deque<Wood> woods) {
+        int size = woods.size();
+        while (size-- > 0) {
+            Wood wood = woods.pollFirst();
 
             if (area[wood.x][wood.y] >= wood.z) {
                 area[wood.x][wood.y] -= wood.z;
-                tmp.offer(new Wood(wood.x, wood.y, wood.z+1));
+                wood.z++;
+                woods.offerLast(wood);
             }
             else {
-                diedWood.offer(new Wood(wood.x, wood.y, wood.z));
+                diedWood.offerLast(wood);
             }
         }
-
-        return tmp;
     }
 }
