a2c3c5f58676a317e0e67cacfeb2b67507e1929e
syann97 <shallom75321@gmail.com>
2026-03-26 14:33:31 +0900

    [Gold V] Title: 화학 실험, Time: 420 ms, Memory: 57272 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/20311. 화학 실험/화학 실험.java
+++ 백준/Gold/20311. 화학 실험/화학 실험.java
@@ -4,24 +4,41 @@ import java.io.IOException;
 import java.util.*;
 
 
+class Color implements Comparable<Color> {
+	int index;
+	int count;
+
+	public Color (int index, int count) {
+		this.index = index;
+		this.count = count;
+	}
+
+	@Override
+	public int compareTo (Color o) {
+		return o.count - this.count;
+	}
+}
+
 public class Main {
 	static int N;
 	static int K;
 	static StringTokenizer st;
 	static int[] arr;
-	static int[] c;
+	static Color[] c;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		c = new int[K];
-
+		c = new Color[K];
 
 		st = new StringTokenizer(br.readLine());
+		int index = 1;
 		for (int i = 0; i < K; i++) {
-			c[i] = Integer.parseInt(st.nextToken());
+			int count = Integer.parseInt(st.nextToken());
+			c[i] = new Color(index, count);
+			index++;
 		}
 
 		Arrays.sort(c);
@@ -34,33 +51,27 @@ public class Main {
 				sb.append(arr[i]).append(" ");
 			}
 		}
-
 		System.out.println(sb);
 	}
 
 	static boolean getAnswer() {
-		int cIndex = K-1;
+		if (c[0].count * (long)2 > N + 1) return false;
+
 		int pIndex = 0;
 		arr = new int[N];
-		int colorNumber = 1;
 
-		if (c[cIndex] * 2 > N + 1) return false;
-
-		while (cIndex >= 0) {
-			int insertCount = c[cIndex];
+		for (int i = 0; i < K; i++) {
+			int insertCount = c[i].count;
+			int insertIndex = c[i].index;
 
 			while (insertCount-- > 0) {
-				arr[pIndex] = colorNumber;
+				arr[pIndex] = insertIndex;
 				pIndex += 2;
 				if (pIndex >= N) {
 					pIndex = 1;
 				}
 			}
-
-			colorNumber++;
-			cIndex--;
 		}
-
 		return true;
 	}
 }
