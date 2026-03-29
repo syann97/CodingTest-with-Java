7b0ef037c3274ef3e608c9b2ac747a5e20756780
syann97 <shallom75321@gmail.com>
2026-03-29 15:47:38 +0900

    [Gold IV] Title: 로또, Time: 76 ms, Memory: 12220 KB -BaekjoonHub

--- 백준/Gold/2758. 로또/로또.java
+++ 백준/Gold/2758. 로또/로또.java
@@ -1,34 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static StringTokenizer st;
-    static StringBuilder sb;
-    static final int N = 10;
-    static final int M = 2000;
-    static long[][] dp = new long[N+1][M+1];
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuilder();
+	static StringTokenizer st;
+	static long[][] dp;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
 
-        for (int i = 1; i <= M; i++) dp[1][i] = i;
+		init();
 
-        for (int i = 2; i <= N; i++) {
-            for (int j = (int)Math.pow(2, i-1); j <= M; j++) {
-                dp[i][j] = dp[i-1][j/2] + dp[i][j-1];
-            }
-        }
+		while (T-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
 
-        int T = Integer.parseInt(br.readLine());
-        while (T --> 0) {
-            st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            sb.append(dp[n][m]).append("\n");
-        }
-        System.out.println(sb);
-    }
+			sb.append(dp[n-1][m]).append("\n");
+		}
+		System.out.print(sb);
+	}
+
+	static void init() {
+		dp = new long[10][2001];
+
+		for (int i = 1; i <= 2000; i++) {
+			dp[0][i] = i;
+		}
+
+		for (int i = 1; i < 10; i++) {
+			for (int j = 1 << i; j <= 2000; j++) {
+				dp[i][j] = dp[i - 1][j / 2] + dp[i][j - 1];
+			}
+		}
+	}
 }
