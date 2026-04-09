8baf17f87d80e65240262561f76b70f2ec4489cc
syann97 <shallom75321@gmail.com>
2026-04-09 19:48:56 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -3,14 +3,19 @@ SELECT
     I.ITEM_NAME,
     I.RARITY
 FROM
-    ITEM_INFO I
-JOIN
-    ITEM_TREE T ON I.ITEM_ID = T.ITEM_ID
+    ITEM_INFO I JOIN ITEM_TREE T
+        ON I.ITEM_ID = T.ITEM_ID
 WHERE
-    T.PARENT_ITEM_ID IN (
-        SELECT ITEM_ID 
-        FROM ITEM_INFO 
-        WHERE RARITY = 'RARE'
-    )
+    T.PARENT_ITEM_ID 
+    IN (SELECT 
+            ITEM_ID
+        FROM
+            ITEM_INFO
+        WHERE
+            RARITY = 'RARE'
+       )
 ORDER BY
-    I.ITEM_ID DESC;
+    I.ITEM_ID DESC
+     
+    
+    
