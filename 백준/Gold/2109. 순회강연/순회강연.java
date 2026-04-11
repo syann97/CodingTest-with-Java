985ccb47d09356538716b6e082781f221b46e84f
syann97 <shallom75321@gmail.com>
2026-04-11 15:00:09 +0900

    [Gold III] Title: 순회강연, Time: 296 ms, Memory: 23460 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.java
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.PriorityQueue;
@@ -7,20 +8,63 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static StringTokenizer st;
+	static PriorityQueue<int[]> pq;
+	static PriorityQueue<Integer> compPq;
+	static int max;
+
 	public static void main(String[] args) throws Exception {
+		init();
+		greedy();
+	}
+
+	static void greedy() {
+		int day = max;
+		int answer = 0;
+		boolean flag = false;
+
+		while (day > 0) {
+
+			if (!flag) {
+				while (true) {
+					if (pq.isEmpty()) {
+						flag = true;
+						break;
+					}
+					if (pq.peek()[0] < day) break;
+					compPq.offer(pq.poll()[1]);
+				}
+			}
+			else {
+				if (day >= compPq.size()) {
+					while (!compPq.isEmpty()) {
+						answer += compPq.poll();
+					}
+					break;
+				}
+			}
+
+			if (!compPq.isEmpty()) answer += compPq.poll();
+
+			day--;
+		}
+
+		System.out.println(answer);
+	}
+
+	static void init() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 
-		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
-            if (o1[0] == o2[0]) {
-                return o2[1] - o1[1];
-            }
-            return o2[0] - o1[0];
-        });
+		pq = new PriorityQueue<>((o1, o2) -> {
+			if (o1[0] == o2[0]) {
+				return o2[1] - o1[1];
+			}
+			return o2[0] - o1[0];
+		});
 
-		PriorityQueue<Integer> compPq = new PriorityQueue<>((o1, o2) -> o2 - o1);
+		compPq = new PriorityQueue<>((o1, o2) -> o2 - o1);
 
-		int max = 0;
+		max = 0;
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 
@@ -30,21 +74,5 @@ public class Main {
 			pq.offer(new int[]{d, p});
 			max = Math.max(max, d);
 		}
-
-		int day = max;
-		int answer = 0;
-
-		while (day > 0) {
-
-			while (!pq.isEmpty() && pq.peek()[0] >= day) {
-				compPq.offer(pq.poll()[1]);
-			}
-
-			if (!compPq.isEmpty()) answer += compPq.poll();
-
-			day--;
-		}
-
-		System.out.println(answer);
 	}
 }
