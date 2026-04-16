896756b6f5ddcfbfb5282bad4cb4e0b78eee4e14
syann97 <shallom75321@gmail.com>
2026-04-16 15:57:06 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 6.93 ms, Memory: 90.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    static Map<String, Integer> order;
+    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+        order = new HashMap<>();
+        order.put("code", 0);
+        order.put("date", 1);
+        order.put("maximum", 2);
+        order.put("remain", 3);
+        
+        // data = [code, date, maximum, remain]
+        
+        ArrayList<int[]> validData = new ArrayList<>();
+        
+        for (int[] d : data) {
+            if (d[order.get(ext)] < val_ext) {
+                validData.add(d);
+            }
+        }
+        
+        
+        
+        return sort(validData, sort_by);
+    }
+    
+    static int[][] sort(ArrayList<int[]> validData, String sort_by) {
+        Collections.sort(validData, (o1, o2) -> {
+           return o1[order.get(sort_by)] - o2[order.get(sort_by)];
+        });
+        
+        int[][] sortedArray = new int[validData.size()][4];
+        for (int i = 0; i < validData.size(); i++) {
+            sortedArray[i] = validData.get(i);
+        }
+        
+        return sortedArray;
+    }
+}
