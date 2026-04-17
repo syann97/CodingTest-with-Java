7dd06bfd3c82aaf822102f2a71204b86c3439c39
syann97 <shallom75321@gmail.com>
2026-04-17 17:30:35 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 244 ms, Memory: 36916 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -1,43 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
-
-class Main {
+public class Main {
     static StringTokenizer st;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
 
+        int[] a = new int[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            a[i] = Integer.parseInt(st.nextToken());
         }
 
-        int start = 0;
-        int end = 0;
-        int max = 1;
+        int s = 0;
+        int e = 0;
+        int[] count = new int[100001];
 
-        int[] counting = new int[100001];
-
-        while (end < N) {
-            int num = arr[end];
-            if (counting[num] == K) {
-                counting[arr[start]]--;
-                start++;
+        int answer = 0;
+        while (s < N && e < N) {
+            if (++count[a[e]] > K) {
+                while (count[a[e]] > K) {
+                    count[a[s++]]--;
+                }
             }
-
-            else {
-                counting[num]++;
-                end++;
-                max = Math.max(max, end - start);
-            }
+            e++;
+            answer = Math.max(answer, e-s);
         }
 
-        System.out.println(max);
+        System.out.println(answer);
     }
 }
