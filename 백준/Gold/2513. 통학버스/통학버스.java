cd95cc54dfee49afe3fc38b3c59ac9de2706ebda
syann97 <shallom75321@gmail.com>
2026-04-20 16:34:45 +0900

    [Gold III] Title: 통학버스, Time: 240 ms, Memory: 22344 KB -BaekjoonHub

--- 백준/Gold/2513. 통학버스/통학버스.java
+++ 백준/Gold/2513. 통학버스/통학버스.java
@@ -51,25 +51,42 @@ public class Main {
     }
 
     private static int greedy(PriorityQueue<Apart> pq, int K) {
-        int move = 0;
         Apart current = null;
+        int move = 0;
+
+        // 묶음 단위 처리
         while (!pq.isEmpty()) {
+            // 무조건 이번 루프에 이거 처리한다.
             current = pq.poll();
-            int times = ((current.count + K - 1) / K);
-            move += current.pos * times * 2;
 
-            int canTake = K * times;
-            canTake -= current.count;
+            // 그러면 몇 번 왕복하는지 체킹
+            // 1~K까진 1로 나오게 해야되는데  / K를 하면 0이 나오고 K에서는 1이 나오네
+            // current.count+K-1 로 K단위에서 엣지 처리와 +1 처리를 수행하자
+            int aroundCount = (current.count + K -1) / K;
 
-            while (!pq.isEmpty() && pq.peek().count <= canTake) {
-                canTake -= pq.poll().count;
-            }
+            // 일단 순회하는 만큼 move를 더하자
+            move += (current.pos * aroundCount) * 2;
+            // 그러면 이제 몇 번 순회하는지 알았으니 용량을 체킹하자 얼마가 남지?
+            int rest = K * aroundCount - current.count;
 
-            if (canTake > 0 && !pq.isEmpty()) {
+            // 이제 남은 rest에 대해 이번 순회에 모두 처리하도록 하자
+            // 우선 rest보다 작은 왕복 카운트에 대해서는 제거해버리자
+            while (!pq.isEmpty() && rest > 0) {
                 current = pq.peek();
-                current.count -= canTake;
+                // 더 크거나 같은건 빼줘
+                if (rest >= current.count) {
+                    rest -= current.count;
+                    pq.poll();
+                }
+
+                // rest보다 큰 건 count를 빼줘
+                else {
+                    current.count -= rest;
+                    rest = 0;
+                }
             }
         }
+
         return move;
     }
 }
