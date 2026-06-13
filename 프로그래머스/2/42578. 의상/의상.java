d93ba1afd2d10a89b6598d98017af6a24de1fbe6
syann97 <shallom75321@gmail.com>
2026-06-13 15:51:59 +0900

    [level 2] Title: 의상, Time: 0.44 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,17 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
-        
         Map<String, Integer> map = new HashMap<>();
+        
         for (String[] cloth : clothes) {
-            String parts = cloth[1];
-            
-            map.put(parts, map.getOrDefault(parts, 0) + 1);
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
         }
         
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            answer *= (entry.getValue() + 1);
+        int answer = 1;
+        for (int value : map.values()) {
+            answer *= (value + 1);
         }
         
         return answer - 1;
